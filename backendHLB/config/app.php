ef68dbd7a6eac541926346b09ff4c66854d7b632
Carlos Sesme Vera <carlossesme31@gmail.com>
2020-01-31 10:12:02 -0500

    modificaciones necesarias para despliegue en heroku

--- backendHLB/config/app.php
+++ backendHLB/config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('base64:k+SjC4cKU1th0Q0/6gCgoX9I8mf7P++AKGoqnBTwMFc='),
 
     'cipher' => 'AES-256-CBC',
 
